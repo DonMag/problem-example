1dae6304ff513163752d98b8e0878a84ac9e3300
ibrahim beltagy <i.beltagy@dcodes.co>
2017-09-27 16:16:13 +0200

    Initial Commit

---
+++ problem example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  problem example
+//
+//  Created by ibrahim beltagy on 9/27/17.
+//  Copyright © 2017 ibrahim beltagy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
